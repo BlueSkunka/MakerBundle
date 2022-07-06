684e60e95dfca9a60d584b0243c3cb298b70b423
Skunka <quentin.somveille3874@gmail.com>
2022-07-06 22:53:17 +0200

    feat: initial commit

---
+++ src/Maker/SkunkaMakeCrud.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Skunka\MakerBundle\Maker;
+
+use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
+
+class SkunkaMakeCrud extends AbstractMaker
+{
+
+}
---
+++ src/SkunkaMakerBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Skunka\MakerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SkunkaMakerBundle extends AbstractBundle
+{
+    
+}
